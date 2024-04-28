d0659dba87085341252ef593027b4c1b6ba227f4
Danny Terefe <48824483+AlAswaad99@users.noreply.github.com>
2024-04-28 20:17:50 +0000

    rename T'Todo' interface to 'ITodo'

--- hooks/use-selected-todo.tsx
+++ hooks/use-selected-todo.tsx
@@ -1,12 +1,12 @@
-import { Todo } from "@/types/types"
+import { ITodo } from "@/types/types"
 import { create } from "zustand"
 
 interface useSelectedTodoProps {
-  todo: Todo | null
-  setTodo: (todo: Todo | null) => void
+  todo: ITodo | null
+  setTodo: (todo: ITodo | null) => void
 }
 
 export const useSelectedTodo = create<useSelectedTodoProps>((set) => ({
   todo: null,
-  setTodo: (todo: Todo | null) => set({ todo }),
+  setTodo: (todo: ITodo | null) => set({ todo }),
 }))
